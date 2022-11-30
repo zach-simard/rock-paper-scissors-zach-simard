11a0223f825493ffb7458192bf3594b60bd3a15d
zachary ladouceur-simard <118993100+zach-simard@users.noreply.github.com>
2022-11-30 12:39:24 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let tool = 0
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (tool == 1) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
